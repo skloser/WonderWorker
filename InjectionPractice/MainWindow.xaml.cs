eedb21066165261be8bddb0ee525dbd8697f6e06
skloser <krum.bagashev@gmail.com>
2015-08-17 23:01:28 +0300

    Full

---
+++ InjectionPractice/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InjectionPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SoftUniEntities softuniContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+            softuniContext = new SoftUniEntities();
+        }
+
+        private void buttonPesho_Click(object sender, RoutedEventArgs e)
+        {
+            string address = this.textboxPesho.Text;
+            softuniContext.Database.ExecuteSqlCommand("INSERT into Addresses (AddressText, TownID) VALUES ('" + address + "', 4)");
+            softuniContext.SaveChanges();
+        }
+    }
+}
